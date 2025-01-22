3ea4d119b1dda76283a79c3ff5f3eec2ed62ec1e
sanchezvvictor <victor.sanchez@meteo.fr>
2025-01-22 11:30:35 +0100

    correcting error from dataloader previous modif

--- core/dataloader.py
+++ core/dataloader.py
@@ -219,18 +219,33 @@ class DateDataloader(DataLoader):
     def __next__(self):
         if self.current_index < self._data_length:
             try:
-                fake_samples = self.fake_dataset[self.current_index]
-
-                obs_samples = self.obs_dataset[self.current_index]
-
-                real_samples = self.real_dataset[self.current_index]
-                
-                self.current_index += min(1, self._data_length - self.current_index)
-
-                return fake_samples, real_samples, obs_samples
+                fake_samples = np.array(
+                    [
+                        self.fake_dataset[self.current_index + i]
+                        for i in range(self.batch_size)
+                    ]
+                )
+                obs_samples = np.array(
+                    [
+                        self.obs_dataset[self.current_index + i]
+                        for i in range(self.batch_size)
+                    ]
+                )
+                real_samples = np.array(
+                    [
+                        self.real_dataset[self.current_index + i]
+                        for i in range(self.batch_size)
+                    ]
+                )
+                self.current_index += min(
+                    self.batch_size, self._data_length - self.current_index
+                )
+                return fake_samples[0], real_samples[0], obs_samples
             except FileNotFoundError as e:
                 logging.warning(f"{self.name} :  File not found, {e}")
-                self.current_index += min(1, self._data_length - self.current_index)
+                self.current_index += min(
+                    self.batch_size, self._data_length - self.current_index
+                )
                 return None, None, None
         else:
             raise StopIteration
@@ -287,10 +302,22 @@ class RandomDataloader(DataLoader):
 
     def __next__(self):
         if self.current_index < self._data_length:
-            fake_samples = self.fake_dataset[self.current_index]
-            real_samples = self.real_dataset[self.current_index]
-            self.current_index += min(1, self._data_length - self.current_index)
-            return fake_samples, real_samples, None
+            fake_samples = np.array(
+                [
+                    self.fake_dataset[self.current_index + i]
+                    for i in range(self.fake_dataset.batch_size)
+                ]
+            )
+            real_samples = np.array(
+                [
+                    self.real_dataset[self.current_index + i]
+                    for i in range(self.real_dataset.batch_size)
+                ]
+            )
+            self.current_index += min(
+                self.batch_size, self._data_length - self.current_index
+            )
+            return fake_samples[0], real_samples[0], None
         else:
             raise StopIteration
 
@@ -339,15 +366,34 @@ class ModDataloader(DateDataloader):
     def __next__(self):
         if self.current_index < self._data_length:
             try:
-                fake_samples = self.fake_dataset[self.current_index]["fake"]
+                fake_samples = np.array(
+                    [
+                        self.fake_dataset[self.current_index + i]["fake"]
+                        for i in range(self.fake_dataset.batch_size)
+                    ]
+                )
+                mod_samples = np.array(
+                    [
+                        self.fake_dataset[self.current_index + i]["mod"]
+                        for i in range(self.fake_dataset.batch_size)
+                    ]
+                )
+                real_samples = np.array(
+                    [
+                        self.real_dataset[self.current_index + i]
+                        for i in range(self.real_dataset.batch_size)
+                    ]
+                )
+                obs_samples = np.array(
+                    [
+                        self.obs_dataset[self.current_index + i]
+                        for i in range(self.batch_size)
+                    ]
+                )
 
-                mod_samples = self.fake_dataset[self.current_index]["mod"]
-
-                real_samples = self.real_dataset[self.current_index]
-
-                obs_samples = self.obs_dataset[self.current_index]
-
-                self.current_index += min(1, self._data_length - self.current_index)
+                self.current_index += min(
+                    self.batch_size, self._data_length - self.current_index
+                )
                 logging.debug(
                     f"mod'ing with option constant_debias set to {self.constant_debias}"
                 )
@@ -358,27 +404,31 @@ class ModDataloader(DateDataloader):
                     f"indices {self.real_dataset.var_indices}, shape {real_samples.shape}"
                 )
                 mod_bias = (
-                    mod_samples.mean(axis=(0, -2, -1))
-                    - real_samples[:, self.real_dataset.var_indices, :, :].mean(
+                    mod_samples[0].mean(axis=(0, -2, -1))
+                    - real_samples[0][:, self.real_dataset.var_indices, :, :].mean(
                         axis=(0, -2, -1)
                     )
                     if self.constant_debias
-                    else mod_samples.mean(axis=0)
-                    - real_samples[:, self.real_dataset.var_indices, :, :].mean(axis=0)
+                    else mod_samples[0].mean(axis=0)
+                    - real_samples[0][:, self.real_dataset.var_indices, :, :].mean(
+                        axis=0
+                    )
                 )
                 if self.constant_debias:
                     logging.debug(f"applying bias {mod_bias}")
-                fake_samples = (
-                    fake_samples - mod_bias[np.newaxis, :, np.newaxis, np.newaxis]
+                fake_samples[0] = (
+                    fake_samples[0] - mod_bias[np.newaxis, :, np.newaxis, np.newaxis]
                     if self.constant_debias
-                    else fake_samples - mod_bias[np.newaxis]
+                    else fake_samples[0] - mod_bias[np.newaxis]
                 )
                 logging.debug(f"mod'ing done")
             except FileNotFoundError as e:
                 logging.warning(f"{self.name} :  File not found, {e}")
-                self.current_index += min(1, self._data_length - self.current_index)
+                self.current_index += min(
+                    self.batch_size, self._data_length - self.current_index
+                )
                 return None, None, None
-            return fake_samples, real_samples, obs_samples
+            return fake_samples[0], real_samples[0], obs_samples
         else:
             raise StopIteration
 
